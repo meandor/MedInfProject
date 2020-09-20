466dfdc83586b64d2922bed53e47ee202c4e3293
Daniel Schruhl <danielschruhl@gmail.com>
2020-09-20 23:22:28 +0200

    Check if redirected to login page

--- e2e/cypress/integration/login.spec.js
+++ e2e/cypress/integration/login.spec.js
@@ -4,6 +4,7 @@ context('login', () => {
   })
 
   it('should have login form', () => {
+    cy.url().should('include', '/login')
     cy.get('#email')
       .type('fake@email.com')
       .should('have.value', 'fake@email.com')

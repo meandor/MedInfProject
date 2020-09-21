b500ced36620d02638ffe61c7190fcecc37a0541
Daniel Schruhl <danielschruhl@gmail.com>
2020-09-21 12:11:49 +0200

    Add login e2e test that actually logs in

--- e2e/cypress/integration/login.spec.js
+++ e2e/cypress/integration/login.spec.js
@@ -3,10 +3,18 @@ context('login', () => {
     cy.visit('/')
   })
 
-  it('should have login form', () => {
-    cy.url().should('include', '/login')
-    cy.get('#email')
+  it('should login', () => {
+    cy.url().should('include', '/login');
+
+    cy.get('input[data-testid=email]')
       .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
+      .should('have.value', 'fake@email.com');
+
+    cy.get('input[data-testid=password]')
+      .type('password')
+      .should('have.value', 'password')
+      .type('{enter}');
+
+    cy.url().should('include', '/dashboard');
   })
 })

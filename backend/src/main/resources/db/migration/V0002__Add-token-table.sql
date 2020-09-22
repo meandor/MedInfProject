d6355b0961ec2484ae60b8a2c2ccd3c8e6bacb2f
Daniel Schruhl <danielschruhl@gmail.com>
2020-09-22 15:59:46 +0200

    Add unique constraint to token table

--- backend/src/main/resources/db/migration/V0002__Add-token-table.sql
+++ backend/src/main/resources/db/migration/V0002__Add-token-table.sql
@@ -2,5 +2,6 @@ CREATE TABLE "tokens"
 (
     user_id    UUID      NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    expires_at TIMESTAMP
+    expires_at TIMESTAMP,
+    CONSTRAINT unique_token UNIQUE (user_id, created_at)
 );

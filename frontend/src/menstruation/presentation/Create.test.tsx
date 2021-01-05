89724a6363a6a4d47ce53f924bec4367400f0763
Daniel Schruhl <danielschruhl@gmail.com>
2021-01-05 09:43:49 +0100

    Add error handler for rejections

--- frontend/src/menstruation/presentation/Create.test.tsx
+++ frontend/src/menstruation/presentation/Create.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render } from '@testing-library/react';
+import {act, fireEvent, render} from '@testing-library/react';
 import { Create } from './Create';
 import { Calendar, Interval } from './Calendar';
 import { createPeriod, Period } from '../domain/menstruationService';
@@ -9,18 +9,14 @@ jest.mock('./Calendar');
 jest.mock('../domain/menstruationService');
 
 describe('Create component', () => {
-  let historyMock: any;
-  let pushMock: jest.Mock;
-  let title: HTMLElement;
-  let calendar: HTMLElement;
-  let saveButton: HTMLElement;
-  let cancelButton: HTMLElement;
-  const calendarMock = Calendar as jest.Mock<JSX.Element>;
   const createPeriodMock = createPeriod as jest.Mock<Promise<Period>>;
   const periodInterval: Interval = {
     start: new Date(2021, 1, 1),
     end: new Date(2021, 1, 5),
   };
+  const calendarMock = Calendar as jest.Mock<JSX.Element>;
+  let historyMock: { push: jest.Mock };
+  let pushMock: jest.Mock;
 
   beforeEach(() => {
     pushMock = jest.fn();
@@ -29,41 +25,82 @@ describe('Create component', () => {
     };
     calendarMock.mockImplementation(({ intervalSelectionFn }) => {
       const update: () => JSX.Element = () =>
-        intervalSelectionFn(periodInterval);
+          intervalSelectionFn(periodInterval);
       return (
-        <section data-testid="calendar" onClick={update} aria-hidden="true" />
+          <section data-testid="calendar" onClick={update} aria-hidden="true" />
       );
     });
-    const { getByText, getByTestId } = render(<Create history={historyMock} />);
-    title = getByText(/insert/i);
-    calendar = getByTestId(/calendar/i);
-    saveButton = getByText(/save/i);
-    cancelButton = getByText(/cancel/i);
   });
 
-  it('should render create form', () => {
-    expect(title).toBeInTheDocument();
-    expect(calendar).toBeInTheDocument();
-    expect(saveButton).toBeInTheDocument();
-    expect(cancelButton).toBeInTheDocument();
-  });
 
-  it('should go back when cancel is clicked', async () => {
-    await fireEvent.click(cancelButton);
+  describe('valid behaviour', () => {
+    let title: HTMLElement;
+    let calendar: HTMLElement;
+    let saveButton: HTMLElement;
+    let cancelButton: HTMLElement;
 
-    await expect(historyMock.push).toBeCalledWith('/dashboard');
-  });
-
-  it('should save given interval', async () => {
-    createPeriodMock.mockResolvedValue({
-      start: new Date(),
-      end: new Date(),
+    beforeEach(() => {
+      pushMock = jest.fn();
+      historyMock = {
+        push: pushMock,
+      };
+      calendarMock.mockImplementation(({ intervalSelectionFn }) => {
+        const update: () => JSX.Element = () =>
+            intervalSelectionFn(periodInterval);
+        return (
+            <section data-testid="calendar" onClick={update} aria-hidden="true" />
+        );
+      });
+      const { getByText, getByTestId } = render(<Create history={historyMock} />);
+      title = getByText(/insert/i);
+      calendar = getByTestId(/calendar/i);
+      saveButton = getByText(/save/i);
+      cancelButton = getByText(/cancel/i);
     });
 
-    await fireEvent.click(calendar);
-    await fireEvent.click(saveButton);
+    it('should render create form', () => {
+      expect(title).toBeInTheDocument();
+      expect(calendar).toBeInTheDocument();
+      expect(saveButton).toBeInTheDocument();
+      expect(cancelButton).toBeInTheDocument();
+    });
 
-    await expect(createPeriodMock).toBeCalledWith(periodInterval);
-    await expect(pushMock).toBeCalledWith('/dashboard');
+    it('should go back when cancel is clicked', async () => {
+      await fireEvent.click(cancelButton);
+
+      await expect(historyMock.push).toBeCalledWith('/dashboard');
+    });
+
+    it('should save given interval', async () => {
+      createPeriodMock.mockResolvedValue({
+        start: new Date(),
+        end: new Date(),
+      });
+
+      await fireEvent.click(calendar);
+      await fireEvent.click(saveButton);
+
+      await expect(createPeriodMock).toBeCalledWith(periodInterval);
+      await expect(pushMock).toBeCalledWith('/dashboard');
+    });
+  });
+
+  describe('error handling', () => {
+    it('should show creation error',  () => {
+       return act(async () => {
+        createPeriodMock.mockRejectedValue(new Error('foo'));
+        const { getByText,getByTestId } = render(
+            <Create history={historyMock} />
+        );
+        const calendar = getByTestId(/calendar/i);
+        const saveButton = getByText(/save/i);
+
+        await fireEvent.click(calendar);
+        await fireEvent.click(saveButton);
+
+        await expect(createPeriodMock).toBeCalledWith(periodInterval);
+        await expect(getByTestId(/error/i)).toHaveTextContent(/error/i);
+      });
+    });
   });
 });
--- frontend/src/menstruation/presentation/Create.tsx
+++ frontend/src/menstruation/presentation/Create.tsx
@@ -2,6 +2,8 @@ import React, { FormEvent, useState } from 'react';
 import { Calendar, Interval } from './Calendar';
 import './create.scss';
 import { createPeriod } from '../domain/menstruationService';
+import { logger } from '../../logger';
+import {ErrorInfo} from "../../core/presentation/ErrorInfo";
 
 export function Create({
   history,
@@ -9,16 +11,26 @@ export function Create({
   history: { push: (_: string) => any };
 }): JSX.Element {
   const [interval, setInterval] = useState<undefined | Interval>(undefined);
+  const [errorMessage, setErrorMessage] = useState<string>('');
   const onSubmitHandler: any = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setErrorMessage('');
     if (interval) {
-      return createPeriod(interval).then(history.push('/dashboard'));
+      return createPeriod(interval)
+        .then(history.push('/dashboard'))
+        .catch((error) => {
+          logger.error('Was not able to create period', error);
+          setErrorMessage(
+            'There was an error creating that period. Please try again or check back later.'
+          );
+        });
     }
   };
 
   return (
     <section className="create">
       <h1 className="create__header">Insert Period</h1>
+      <ErrorInfo errorMessage={errorMessage} data-testid="error" />
       <form onSubmit={onSubmitHandler}>
         <Calendar
           data-testid="calendar"

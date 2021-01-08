4391efe3dba071698d1a29eb857af2e6b709cabb
Daniel Schruhl <danielschruhl@gmail.com>
2021-01-08 10:13:08 +0100

    Format

--- backend/src/main/resources/db/migration/V0003__Add-menstruation-table.sql
+++ backend/src/main/resources/db/migration/V0003__Add-menstruation-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "menstruation"
 (
-    user_id    UUID      NOT NULL,
+    user_id    UUID NOT NULL,
     start_date DATE NOT NULL,
     end_date   DATE NOT NULL,
     CONSTRAINT unique_menstruation UNIQUE (user_id, start_date, end_date)
